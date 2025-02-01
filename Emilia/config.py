c84cb85580897a9355caa21e844718e6bc939c47
alcyonebots <evedhatachi@gmail.com>
2025-02-01 15:47:58 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,26 +8,26 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "22878444" # API_HASH from my.telegram.org
+    API_ID = 550641aa3600a98c1cb94afc259f2244 # API_ID from my.telegram.org
 
-    BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Elf_Robot" # BOT_USERNAME
+    BOT_ID = 7887377098 # BOT_ID
+    BOT_USERNAME = "AkenoHajimeBot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://Akeno:Hajime@akeno.zyn9h.mongodb.net/" # MongoDB URL from MongoDB Atlas
 
-    SUPPORT_CHAT = "SpiralTechDivision" # Support Chat Username
-    UPDATE_CHANNEL = "SpiralUpdates" # Update Channel Username
+    SUPPORT_CHAT = "Alcyone_Support" # Support Chat Username
+    UPDATE_CHANNEL = "AlcyoneBots" # Update Channel Username
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
-    TOKEN = "57375" # Bot Token from @BotFather
+    DEV_USERS = [6663845789, 6698364560] # Dev Users
+    TOKEN = "7887377098:AAGPD37rvqymDGiv3R2xXXvqL4p70GxnWB8" # Bot Token from @BotFather
     CLONE_LIMIT = 50 # Number of clones your bot can make
 
-    EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    EVENT_LOGS = -1002183841044 # Event Logs Chat ID
+    OWNER_ID = 6663845789 # Owner ID
  
-    TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
-    BOT_NAME = "Emilia" # Bot Name
+    TEMP_DOWNLOAD_DIRECTORY = "root/" # Temporary Download Directory
+    BOT_NAME = "Akeno" # Bot Name
     WALL_API = "6950f53" # Wall API from wall.alphacoders.com
     ORIGINAL_EVENT_LOOP = True # Do not Change
 
