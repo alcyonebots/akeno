82429e66b8c48802de57fc71f05f060f4aaa5d23
alcyonebots <evedhatachi@gmail.com>
2025-02-02 08:57:45 +0530

    Update antispam_info.py

--- Emilia/info/antispam_info.py
+++ Emilia/info/antispam_info.py
@@ -4,5 +4,5 @@ __help__ = """
 • /antispam <on/off/enable/disable>: Will toggle our antispam.
 
 Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect you and your groups by removing spam flooders as quickly as possible.
-
+"""
 __mod_name__ = "Anti-Spam"
