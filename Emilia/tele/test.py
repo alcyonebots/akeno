fa24d60df6090209f5356f7cff69759c3af68c97
alcyonebots <evedhatachi@gmail.com>
2025-02-03 17:54:02 +0530

    Delete Emilia/tele/test.py

--- Emilia/tele/test.py
+++
@@ -1,8 +0,0 @@
-from Emilia.custom_filter import register
-from Emilia.utils.decorators import *
-
-
-@register(pattern="test")
-@rate_limit(3, 60)
-async def test(event):
-    await event.reply("test")
